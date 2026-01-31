27c4d90e5604e3bb794ea4b86da7bcf8df1f6937
Brundabana Sahu <sbrundabana12@gmail.com>
2026-01-31 21:45:30 +0530

    Intial commit

---
+++ Assignment-1/Conditionalstatement/liesbet50and100.py
@@ -0,0 +1,5 @@
+num=int(input("Enter a number: "))
+if 50 <=num<=100:
+    print("Lies between 50 and 100")
+else:
+    print("Does not lie between 50 and 100")    
---
+++ Assignment-1/Conditionalstatement/specialchar.py
@@ -0,0 +1,9 @@
+s1=input("Enter the character:")
+if 'A'<= s1 <='Z':
+    print("Uppercase")
+elif 'a'<= s1 <='z':
+    print("Lowercase")
+elif '0'<= s1 <='9':
+    print("Digit")
+else:
+    print("Special Character")            
