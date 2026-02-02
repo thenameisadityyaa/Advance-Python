55d827e251f536a61e7602b81c6d6b44593480b2
Brundabana Sahu <sbrundabana12@gmail.com>
2026-02-02 23:45:52 +0530

    Intail commit

--- Tuples/main.py
+++ Tuples/main.py
@@ -7,3 +7,23 @@ print(type(t1))
 print(t1.count(1))
 print(t1.index("Sagar"))
 
+
+a = 10
+b = 20
+c = 30
+
+t = a, b, c
+print(t)
+
+x, y, z = t
+print(x)
+print(y)
+print(z)
+
+t1 = (1, 2, 3)
+t2 = (4, 5, 6)
+
+t3 = t1 + t2
+print(t3)
+
+
